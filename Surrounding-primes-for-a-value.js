7a7d891815bfbd758c6a285fe1f2d34c3f5e8b5e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-19 15:57:28 -0700

    Create Surrounding-primes-for-a-value.js

---
+++ Surrounding-primes-for-a-value.js
@@ -0,0 +1,15 @@
+/*
+We need a function prime_bef_aft() that gives 
+the largest prime below a certain given value n,
+and the smallest prime larger than this value.
+
+The result should be output in a list like the following:
+  primeBefAft == [befPrime, aftPrime]
+  If n is a prime number it will give two primes, 
+  n will not be included in the result.
+
+Let's see some cases:
+  primeBefAft(100) == [97, 101]
+  primeBefAft(97) == [89, 101]
+  primeBefAft(101) == [97, 103]
+*/
